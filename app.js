41aee8bdd3ca7e6b30415fbdd7761a4ed2c23aeb
Omas Ajiri <omasajiri@gmail.com>
2022-10-28 01:11:23 +0100

    feat: add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+const port = 4500;
+
+app.get("/", (req, res) => {
+  res.send({
+    slackUsername: "Omas Ajiri",
+    backend: true,
+    age: 30,
+    bio: "My name is Omas, am 307 years old and I love technology.",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
